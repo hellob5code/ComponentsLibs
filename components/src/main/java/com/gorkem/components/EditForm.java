f7dcc45665e2a8e8749223b5a3eadda1f100f8b5
Görkem <gkaradagan@gmail.com>
2014-12-22 09:27:31 +0200

    pageindicator from jackwharton

--- components/src/main/java/com/gorkem/components/EditForm.java
+++ components/src/main/java/com/gorkem/components/EditForm.java
@@ -22,22 +22,22 @@ import com.gorkem.components.func.onEditTextChangeListener;
 
 public class EditForm extends LinearLayout {
 
-	public static String TAG = "EditForm";
-	protected EditText etForm;
-	protected TextView tvForm;
-	protected View v;
+    public static String TAG = "EditForm";
+    protected EditText etForm;
+    protected TextView tvForm;
+    protected View v;
 
-	private onEditTextChangeListener mTextListener;
+    private onEditTextChangeListener mTextListener;
 
-	public EditForm(Context context, AttributeSet attr) {
-		super(context, attr);
+    public EditForm(Context context, AttributeSet attr) {
+        super(context, attr);
 
-		inflateLayout(context);
+        inflateLayout(context);
 
-		findViews();
-		baseinit(context, attr);
+        findViews();
+        baseinit(context, attr);
 
-	}
+    }
 
 //	public EditForm(Context context, AttributeSet attrs, int defStyle) {
 //
@@ -48,159 +48,162 @@ public class EditForm extends LinearLayout {
 //
 //	}
 
-	private void inflateLayout(Context context) {
+    private void inflateLayout(Context context) {
 
-		final LayoutInflater inflater = (LayoutInflater) context
-				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		v = inflater.inflate(R.layout.editform, this);
-	}
+        final LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        v = inflater.inflate(R.layout.editform, this);
+    }
 
-	protected void findViews() {
-		this.tvForm = (TextView) findViewById(R.id.tvForm);
+    protected void findViews() {
+        this.tvForm = (TextView) findViewById(R.id.tvForm);
 
-		this.etForm = (EditText) findViewById(R.id.etForm);
-	}
+        this.etForm = (EditText) findViewById(R.id.etForm);
+    }
 
-	protected void baseinit(Context context, AttributeSet attr) {
+    protected void baseinit(Context context, AttributeSet attr) {
 
-		final TypedArray a = context.obtainStyledAttributes(attr,
-				R.styleable.EditForm);
-		String labelText = a.getString(R.styleable.EditForm_labelText);
-		String editText = a.getString(R.styleable.EditForm_editText);
+        final TypedArray a = context.obtainStyledAttributes(attr,
+                R.styleable.EditForm);
+        String labelText = a.getString(R.styleable.EditForm_labelText);
+        String editText = a.getString(R.styleable.EditForm_editText);
 
-		String editHint = a.getString(R.styleable.EditForm_editHint);
+        String editHint = a.getString(R.styleable.EditForm_editHint);
 
-		boolean editEnable = a
-				.getBoolean(R.styleable.EditForm_editEnable, true);
-		boolean editSingleLine = a.getBoolean(
-				R.styleable.EditForm_editSingleLine, false);
-		boolean must = a.getBoolean(R.styleable.EditForm_must, false);
+        boolean editEnable = a
+                .getBoolean(R.styleable.EditForm_editEnable, true);
+        boolean editSingleLine = a.getBoolean(
+                R.styleable.EditForm_editSingleLine, false);
+        boolean must = a.getBoolean(R.styleable.EditForm_must, false);
 
-		float width = a.getDimension(R.styleable.EditForm_labelWidth, 0);
+        float width = a.getDimension(R.styleable.EditForm_labelWidth, 0);
 
-		float labelSize = a.getDimension(R.styleable.EditForm_labelSize, 0);
-		float editSize = a.getDimension(R.styleable.EditForm_editSize, 0);
+        float labelSize = a.getDimension(R.styleable.EditForm_labelSize, 0);
+        float editSize = a.getDimension(R.styleable.EditForm_editSize, 0);
 
-		int labelColor = a.getColor(R.styleable.EditForm_labelcolor, 0);
-		int editColor = a.getColor(R.styleable.EditForm_editcolor, 0);
+        int labelColor = a.getColor(R.styleable.EditForm_labelcolor, 0);
+        int editColor = a.getColor(R.styleable.EditForm_editcolor, 0);
 
-		int inputType = a.getInt(R.styleable.EditForm_android_inputType,
-				EditorInfo.TYPE_CLASS_TEXT);
+        int inputType = a.getInt(R.styleable.EditForm_android_inputType,
+                EditorInfo.TYPE_CLASS_TEXT);
 
-		if (editText != null)
-			etForm.setText(editText);
+        if (editText != null)
+            etForm.setText(editText);
 
-		if (editHint != null)
-			etForm.setHint(editHint);
+        if (editHint != null)
+            etForm.setHint(editHint);
 
-		if (editEnable == false)
-			etForm.setEnabled(false);
+        if (editEnable == false)
+            etForm.setEnabled(false);
 
-		if (editSingleLine == true)
-			etForm.setSingleLine();
+        if (editSingleLine == true)
+            etForm.setSingleLine();
 
-		if (editSize != 0)
-			etForm.setTextSize(TypedValue.COMPLEX_UNIT_PX, editSize);
+        if (editSize != 0)
+            etForm.setTextSize(TypedValue.COMPLEX_UNIT_PX, editSize);
 
-		if (editColor != 0)
-			etForm.setTextColor(editColor);
+        if (editColor != 0)
+            etForm.setTextColor(editColor);
 
-		etForm.setInputType(inputType);
+        etForm.setInputType(inputType);
 
-		if (must) {
-			String styledText = "<font color='red'>*</font>";
-			tvForm.setText(Html.fromHtml(labelText + styledText));
-		} else {
-			if (labelText != null)
-				tvForm.setText(labelText);
-		}
+        if (must) {
+            String styledText = "<font color='red'>*</font>";
+            tvForm.setText(Html.fromHtml(labelText + styledText));
+        } else {
+            if (labelText != null)
+                tvForm.setText(labelText);
+        }
 
-		if (labelSize != 0)
-			tvForm.setTextSize(TypedValue.COMPLEX_UNIT_PX, labelSize);
+        if (labelSize != 0)
+            tvForm.setTextSize(TypedValue.COMPLEX_UNIT_PX, labelSize);
 
-		if (labelColor != 0)
-			tvForm.setTextColor(labelColor);
+        if (labelColor != 0)
+            tvForm.setTextColor(labelColor);
 
-		if (width != 0)
-			tvForm.getLayoutParams().width = (int) width;
+        if (width != 0)
+            tvForm.getLayoutParams().width = (int) width;
 
-		a.recycle();
+        a.recycle();
 
-		etForm.addTextChangedListener(new TextWatcher() {
+        etForm.addTextChangedListener(new TextWatcher() {
 
-			@Override
-			public void onTextChanged(CharSequence s, int start, int before,
-					int count) {
-
-
-			}
-
-			@Override
-			public void beforeTextChanged(CharSequence s, int start, int count,
-					int after) {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before,
+                                      int count) {
 
 
-			}
+            }
 
-			@Override
-			public void afterTextChanged(Editable s) {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count,
+                                          int after) {
 
-				// Notify the listener
-				if (mTextListener != null) {
-					mTextListener.onChanged(s.toString());
-				}
-			}
-		});
-	}
 
-	public void setonEditTextChangeListener(
-			onEditTextChangeListener mTextListener) {
-		this.mTextListener = mTextListener;
-	}
+            }
 
-	public void setEditTextTAG(Object tag) {
-		this.etForm.setTag(tag);
-	}
+            @Override
+            public void afterTextChanged(Editable s) {
 
-	public Object getEditTextTAG() {
-		return etForm.getTag();
-	}
+                // Notify the listener
+                if (mTextListener != null) {
+                    if (s.length() == 0)
+                        mTextListener.onChanged("");
+                    else
+                        mTextListener.onChanged(s.toString());
+                }
+            }
+        });
+    }
 
-	public void setEditTextID(int ID) {
-		this.etForm.setId(ID);
-	}
+    public void setonEditTextChangeListener(
+            onEditTextChangeListener mTextListener) {
+        this.mTextListener = mTextListener;
+    }
 
-	public int getEditTextID() {
-		return this.etForm.getId();
-	}
+    public void setEditTextTAG(Object tag) {
+        this.etForm.setTag(tag);
+    }
 
-	public void setEditTextString(String a) {
-		this.etForm.setText(a);
-	}
+    public Object getEditTextTAG() {
+        return etForm.getTag();
+    }
 
-	public void setTextViewString(String a) {
-		this.tvForm.setText(a);
-	}
+    public void setEditTextID(int ID) {
+        this.etForm.setId(ID);
+    }
 
-	public String getEditTextString() {
-		return this.etForm.getText().toString();
-	}
+    public int getEditTextID() {
+        return this.etForm.getId();
+    }
 
-	public String getTextViewString() {
-		return this.tvForm.getText().toString();
-	}
+    public void setEditTextString(String a) {
+        this.etForm.setText(a);
+    }
 
-	public EditText getEtForm() {
-		return etForm;
-	}
+    public void setTextViewString(String a) {
+        this.tvForm.setText(a);
+    }
 
-	public TextView getTvForm() {
-		return tvForm;
-	}
+    public String getEditTextString() {
+        return this.etForm.getText().toString();
+    }
 
-	public void setEditEnable(boolean i) {
-		this.etForm.setEnabled(i);
+    public String getTextViewString() {
+        return this.tvForm.getText().toString();
+    }
 
-	}
+    public EditText getEtForm() {
+        return etForm;
+    }
+
+    public TextView getTvForm() {
+        return tvForm;
+    }
+
+    public void setEditEnable(boolean i) {
+        this.etForm.setEnabled(i);
+
+    }
 
 }
--- components/src/main/java/com/gorkem/components/RippleView.java
+++ components/src/main/java/com/gorkem/components/RippleView.java
@@ -24,8 +24,6 @@
 
 package com.gorkem.components;
 
-import com.gorkem.components.func.onAnimationListener;
-
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Bitmap;
@@ -45,13 +43,15 @@ import android.view.animation.ScaleAnimation;
 import android.widget.RelativeLayout;
 
 /**
- * Author : Chutaux Robin Date : 10/8/2014
+ * Author :    Chutaux Robin
+ * Date :      10/8/2014
  */
-public class RippleView extends RelativeLayout {
+public class RippleView extends RelativeLayout
+{
     private int WIDTH;
     private int HEIGHT;
     private int FRAME_RATE = 10;
-    private int DURATION = 300;
+    private int DURATION = 400;
     private int PAINT_ALPHA = 90;
     private Handler canvasHandler;
     private float radiusMax = 0;
@@ -73,59 +73,49 @@ public class RippleView extends RelativeLayout {
     private View childView;
     private int ripplePadding;
     private GestureDetector gestureDetector;
-    private onAnimationListener mAnimListener = null;
-
-    public void setonAnimationListener(onAnimationListener _mAnimListener) {
-        mAnimListener = _mAnimListener;
-    }
-
-    private Runnable runnable = new Runnable() {
+    private Runnable runnable = new Runnable()
+    {
         @Override
-        public void run() {
+        public void run()
+        {
             invalidate();
         }
     };
 
-    public RippleView(Context context) {
+    public RippleView(Context context)
+    {
         super(context);
     }
 
-    public RippleView(Context context, AttributeSet attrs) {
+    public RippleView(Context context, AttributeSet attrs)
+    {
         super(context, attrs);
         init(context, attrs);
     }
 
-    public RippleView(Context context, AttributeSet attrs, int defStyle) {
+    public RippleView(Context context, AttributeSet attrs, int defStyle)
+    {
         super(context, attrs, defStyle);
         init(context, attrs);
     }
 
-    private void init(final Context context, final AttributeSet attrs) {
+    private void init(final Context context, final AttributeSet attrs)
+    {
         if (isInEditMode())
             return;
 
-        final TypedArray typedArray = context.obtainStyledAttributes(attrs,
-                R.styleable.RippleView);
-        rippleColor = typedArray.getColor(R.styleable.RippleView_rv_color,
-                getResources().getColor(R.color.rippelColor));
+        final TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.RippleView);
+        rippleColor = typedArray.getColor(R.styleable.RippleView_rv_color, getResources().getColor(R.color.rippelColor));
         rippleType = typedArray.getInt(R.styleable.RippleView_rv_type, 0);
-        hasToZoom = typedArray
-                .getBoolean(R.styleable.RippleView_rv_zoom, false);
-        isCentered = typedArray.getBoolean(R.styleable.RippleView_rv_centered,
-                false);
-        DURATION = typedArray.getInteger(
-                R.styleable.RippleView_rv_rippleDuration, DURATION);
-        FRAME_RATE = typedArray.getInteger(R.styleable.RippleView_rv_framerate,
-                FRAME_RATE);
-        PAINT_ALPHA = typedArray.getInteger(R.styleable.RippleView_rv_alpha,
-                PAINT_ALPHA);
-        ripplePadding = typedArray.getDimensionPixelSize(
-                R.styleable.RippleView_rv_ripplePadding, 0);
+        hasToZoom = typedArray.getBoolean(R.styleable.RippleView_rv_zoom, false);
+        isCentered = typedArray.getBoolean(R.styleable.RippleView_rv_centered, false);
+        DURATION = typedArray.getInteger(R.styleable.RippleView_rv_rippleDuration, DURATION);
+        FRAME_RATE = typedArray.getInteger(R.styleable.RippleView_rv_framerate, FRAME_RATE);
+        PAINT_ALPHA = typedArray.getInteger(R.styleable.RippleView_rv_alpha, PAINT_ALPHA);
+        ripplePadding = typedArray.getDimensionPixelSize(R.styleable.RippleView_rv_ripplePadding, 0);
         canvasHandler = new Handler();
-        zoomScale = typedArray.getFloat(R.styleable.RippleView_rv_zoomScale,
-                1.03f);
-        zoomDuration = typedArray.getInt(
-                R.styleable.RippleView_rv_zoomDuration, 200);
+        zoomScale = typedArray.getFloat(R.styleable.RippleView_rv_zoomScale, 1.03f);
+        zoomDuration = typedArray.getInt(R.styleable.RippleView_rv_zoomDuration, 200);
         paint = new Paint();
         paint.setAntiAlias(true);
         paint.setStyle(Paint.Style.FILL);
@@ -133,35 +123,18 @@ public class RippleView extends RelativeLayout {
         paint.setAlpha(PAINT_ALPHA);
         this.setWillNotDraw(false);
 
-        gestureDetector = new GestureDetector(context,
-                new GestureDetector.SimpleOnGestureListener() {
-                    @Override
-                    public boolean onSingleTapConfirmed(MotionEvent e) {
-                        return true;
-                    }
-
-                    @Override
-                    public boolean onSingleTapUp(MotionEvent e) {
-                        return true;
-                    }
-                });
-
-        this.setonAnimationListener(new onAnimationListener() {
-
+        gestureDetector = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener()
+        {
             @Override
-            public void onAnimationStart() {
-
-                if (mAnimListener != null) {
-                    mAnimListener.onAnimationStart();
-                }
+            public boolean onSingleTapConfirmed(MotionEvent e)
+            {
+                return true;
             }
 
             @Override
-            public void onAnimationFinish() {
-
-                if (mAnimListener != null) {
-                    mAnimListener.onAnimationFinish();
-                }
+            public boolean onSingleTapUp(MotionEvent e)
+            {
+                return true;
             }
         });
 
@@ -169,77 +142,97 @@ public class RippleView extends RelativeLayout {
     }
 
     @Override
-    public void addView(View child, int index, ViewGroup.LayoutParams params) {
+    public void addView(View child, int index, ViewGroup.LayoutParams params)
+    {
         childView = child;
         super.addView(child, index, params);
     }
 
     @Override
-    public void draw(Canvas canvas) {
+    public void draw(Canvas canvas)
+    {
         super.draw(canvas);
-        if (animationRunning) {
-            if (DURATION <= timer * FRAME_RATE) {
-                animationRunning = false;
-                timer = 0;
-                durationEmpty = -1;
-                timerEmpty = 0;
-                canvas.restore();
-                invalidate();
-                return;
-            } else
-                canvasHandler.postDelayed(runnable, FRAME_RATE);
 
-            if (timer == 0)
-                canvas.save();
-
-            canvas.drawCircle(x, y,
-                    (radiusMax * (((float) timer * FRAME_RATE) / DURATION)),
-                    paint);
-
-            paint.setColor(getResources().getColor(
-                    android.R.color.holo_red_light));
-
-            if (rippleType == 1 && originBitmap != null
-                    && (((float) timer * FRAME_RATE) / DURATION) > 0.4f) {
-                if (durationEmpty == -1)
-                    durationEmpty = DURATION - timer * FRAME_RATE;
-
-                timerEmpty++;
-                final Bitmap tmpBitmap = getCircleBitmap((int) ((radiusMax) * (((float) timerEmpty * FRAME_RATE) / (durationEmpty))));
-                canvas.drawBitmap(tmpBitmap, 0, 0, paint);
-                tmpBitmap.recycle();
-            }
-
-            paint.setColor(rippleColor);
-
-            if (rippleType == 1) {
-                if ((((float) timer * FRAME_RATE) / DURATION) > 0.6f)
-                    paint.setAlpha((int) (PAINT_ALPHA - ((PAINT_ALPHA) * (((float) timerEmpty * FRAME_RATE) / (durationEmpty)))));
+        try {
+            if (animationRunning)
+            {
+                if (DURATION <= timer * FRAME_RATE)
+                {
+                    animationRunning = false;
+                    timer = 0;
+                    durationEmpty = -1;
+                    timerEmpty = 0;
+                    canvas.restore();
+                    invalidate();
+                    return;
+                }
                 else
-                    paint.setAlpha(PAINT_ALPHA);
-            } else
-                paint.setAlpha((int) (PAINT_ALPHA - ((PAINT_ALPHA) * (((float) timer * FRAME_RATE) / DURATION))));
+                    canvasHandler.postDelayed(runnable, FRAME_RATE);
 
-            timer++;
+                if (timer == 0)
+                    canvas.save();
+
+
+                canvas.drawCircle(x, y, (radiusMax * (((float) timer * FRAME_RATE) / DURATION)), paint);
+
+                paint.setColor(getResources().getColor(android.R.color.holo_red_light));
+
+                if (rippleType == 1 && originBitmap != null && (((float) timer * FRAME_RATE) / DURATION) > 0.4f)
+                {
+                    if (durationEmpty == -1)
+                        durationEmpty = DURATION - timer * FRAME_RATE;
+
+                    timerEmpty++;
+                    final Bitmap tmpBitmap = getCircleBitmap((int) ((radiusMax) * (((float) timerEmpty * FRAME_RATE) / (durationEmpty))));
+                    canvas.drawBitmap(tmpBitmap, 0, 0, paint);
+                    tmpBitmap.recycle();
+                }
+
+                paint.setColor(rippleColor);
+
+                if (rippleType == 1)
+                {
+                    if ((((float) timer * FRAME_RATE) / DURATION) > 0.6f)
+                        paint.setAlpha((int) (PAINT_ALPHA - ((PAINT_ALPHA) * (((float) timerEmpty * FRAME_RATE) / (durationEmpty)))));
+                    else
+                        paint.setAlpha(PAINT_ALPHA);
+                }
+                else
+                    paint.setAlpha((int) (PAINT_ALPHA - ((PAINT_ALPHA) * (((float) timer * FRAME_RATE) / DURATION))));
+
+                timer++;
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            animationRunning = false;
+            timer = 0;
+            durationEmpty = -1;
+            timerEmpty = 0;
+            canvas.restore();
+            invalidate();
+            
         }
+
     }
 
     @Override
-    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+    protected void onSizeChanged(int w, int h, int oldw, int oldh)
+    {
         super.onSizeChanged(w, h, oldw, oldh);
         WIDTH = w;
         HEIGHT = h;
 
-        scaleAnimation = new ScaleAnimation(1.0f, zoomScale, 1.0f, zoomScale,
-                w / 2, h / 2);
+        scaleAnimation = new ScaleAnimation(1.0f, zoomScale, 1.0f, zoomScale, w / 2, h / 2);
         scaleAnimation.setDuration(zoomDuration);
         scaleAnimation.setRepeatMode(Animation.REVERSE);
         scaleAnimation.setRepeatCount(1);
     }
 
     @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        if (gestureDetector.onTouchEvent(event) && !animationRunning) {
+    public boolean onTouchEvent(MotionEvent event)
+    {
+        if (gestureDetector.onTouchEvent(event) && !animationRunning)
+        {
             if (hasToZoom)
                 this.startAnimation(scaleAnimation);
 
@@ -250,10 +243,13 @@ public class RippleView extends RelativeLayout {
 
             radiusMax -= ripplePadding;
 
-            if (isCentered || rippleType == 1) {
+            if (isCentered || rippleType == 1)
+            {
                 this.x = getMeasuredWidth() / 2;
                 this.y = getMeasuredHeight() / 2;
-            } else {
+            }
+            else
+            {
                 this.x = event.getX();
                 this.y = event.getY();
             }
@@ -272,26 +268,26 @@ public class RippleView extends RelativeLayout {
     }
 
     @Override
-    public boolean onInterceptTouchEvent(MotionEvent event) {
+    public boolean onInterceptTouchEvent(MotionEvent event)
+    {
         return true;
     }
 
     private Bitmap getCircleBitmap(final int radius) {
-        final Bitmap output = Bitmap.createBitmap(originBitmap.getWidth(),
-                originBitmap.getHeight(), Bitmap.Config.ARGB_8888);
-        final Canvas canvas = new Canvas(output);
-        final Paint paint = new Paint();
-        final Rect rect = new Rect((int) (x - radius), (int) (y - radius),
-                (int) (x + radius), (int) (y + radius));
 
-        paint.setAntiAlias(true);
-        canvas.drawARGB(0, 0, 0, 0);
-        canvas.drawCircle(x, y, radius, paint);
+            final Bitmap output = Bitmap.createBitmap(originBitmap.getWidth(), originBitmap.getHeight(), Bitmap.Config.ARGB_8888);
+            final Canvas canvas = new Canvas(output);
+            final Paint paint = new Paint();
+            final Rect rect = new Rect((int)(x - radius), (int)(y - radius), (int)(x + radius), (int)(y + radius));
 
-        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
-        canvas.drawBitmap(originBitmap, rect, rect, paint);
+            paint.setAntiAlias(true);
+            canvas.drawARGB(0, 0, 0, 0);
+            canvas.drawCircle(x, y, radius, paint);
+
+            paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+            canvas.drawBitmap(originBitmap, rect, rect, paint);
+
 
         return output;
     }
-
 }
