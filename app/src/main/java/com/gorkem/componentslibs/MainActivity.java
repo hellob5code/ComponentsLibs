e8237a2f390064231694b49aa8bacef693dd2f84
Görkem <gkaradagan@gmail.com>
2015-02-19 17:45:40 +0200

    19.02.2015

--- app/src/main/java/com/gorkem/componentslibs/MainActivity.java
+++ app/src/main/java/com/gorkem/componentslibs/MainActivity.java
@@ -1,39 +1,82 @@
 package com.gorkem.componentslibs;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import com.gorkem.shimmer.Shimmer;
+import com.gorkem.shimmer.ShimmerTextView;
 
-public class MainActivity extends ActionBarActivity {
-
+//ListActivity
+public class MainActivity extends Activity {
+//    ShimmerTextView tv;
+//    Shimmer shimmer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+//        tv = (ShimmerTextView) findViewById(R.id.shimmer_tv);
+
+//        String[] items = getResources().getStringArray(R.array.sample_list);
+//
+//        ArrayAdapter<String> adapter =
+//                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+//        setListAdapter(adapter);
     }
 
+//    public void toggleAnimation(View target) {
+//        if (shimmer != null && shimmer.isAnimating()) {
+//            shimmer.cancel();
+//        } else {
+//            shimmer = new Shimmer();
+//            shimmer.start(tv);
+//        }
+//    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+//    @Override
+//    protected void onListItemClick(ListView l, View v, int position, long id) {
+//        switch (position) {
+//            case 0:
+//                startSignInActivity(false);
+//                break;
+//            case 1:
+//                startSignInActivity(true);
+//                break;
+//            case 2:
+//                startMessageActivity();
+//                break;
+//            case 3:
+//                startUploadActivity();
+//                break;
+//            case 4:
+//                startStateSampleActivity();
+//                break;
+//        }
+//    }
+//
+//    private void startStateSampleActivity() {
+//        Intent intent = new Intent(this, StateSampleActivity.class);
+//        startActivity(intent);
+//    }
+//
+//    private void startUploadActivity() {
+//        Intent intent = new Intent(this, UploadActivity.class);
+//        startActivity(intent);
+//    }
+//
+//    private void startSignInActivity(boolean isEndlessMode) {
+//        Intent intent = new Intent(this, SignInActivity.class);
+//        intent.putExtra(SignInActivity.EXTRAS_ENDLESS_MODE, isEndlessMode);
+//        startActivity(intent);
+//    }
+//
+//    private void startMessageActivity() {
+//        Intent intent = new Intent(this, MessageActivity.class);
+//        startActivity(intent);
+//    }
 }
